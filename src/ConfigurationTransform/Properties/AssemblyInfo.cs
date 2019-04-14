a1a63027a4b175473b67a0c19dd1cea8a7b537bb
golavr <golavr@gmail.com>
2019-04-14 05:17:13 +0300

    Bump assembly version to 3.4

--- src/ConfigurationTransform/Properties/AssemblyInfo.cs
+++ src/ConfigurationTransform/Properties/AssemblyInfo.cs
@@ -29,8 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("3.0.*")]
+[assembly: AssemblyVersion("3.4.*")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: InternalsVisibleTo("ConfigurationTransform_IntegrationTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100670382c34f99fb7b3d17366da877bfed99834ef7f7985f323469c29d44e6ef5845ee36bfd26d1147fdf5a97f355cae78bd697c651a480a7ab272e98bbd1e13b8420a9139151203f2ee4c047fdb6fad032959ff145956dfaea506c638b592adcb932fba6db3ddc061106d9363fb4186613dfc40363f7192da23e6d656f1c61eb1")]
-[assembly: InternalsVisibleTo("ConfigurationTransform_UnitTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100670382c34f99fb7b3d17366da877bfed99834ef7f7985f323469c29d44e6ef5845ee36bfd26d1147fdf5a97f355cae78bd697c651a480a7ab272e98bbd1e13b8420a9139151203f2ee4c047fdb6fad032959ff145956dfaea506c638b592adcb932fba6db3ddc061106d9363fb4186613dfc40363f7192da23e6d656f1c61eb1")]
